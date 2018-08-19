71935e24185f62ba39e95a294bc5b93dc29f3df9
AditiHarrison <40311477+AditiHarrison@users.noreply.github.com>
2018-08-19 17:51:40 -0400

    Add files via upload

---
+++ random forest model.py
@@ -0,0 +1,526 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import seaborn as sns
+
+
+bureau_balance    = pd.read_csv('bureau_balance.csv')
+application_train = pd.read_csv('application_train.csv')
+bureau            = pd.read_csv('bureau.csv')
+credit_bal        = pd.read_csv("credit_card_balance.csv")
+instal_payment    = pd.read_csv("installments_payments.csv")
+POS_CASH_balance  = pd.read_csv("POS_CASH_balance.csv")
+prev_app          = pd.read_csv("previous_application.csv")
+application_test = pd.read_csv('application_test.csv')
+
+
+# Training and Testing data features
+print('Training data shape: ', application_train.shape)
+print('Testing data shape: ', application_test.shape)
+application_train['TARGET'].value_counts()
+
+# Function to calculate missing values by column# Funct 
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+# Missing values statistics
+missing_values = missing_values_table(application_train)
+missing_values.head(20)
+
+# Number of unique classes in each object column (categorical)
+#application_train.dtypes.value_counts()
+
+application_train.select_dtypes(include=['object']).apply(pd.Series.nunique, axis = 0)
+
+# Create a label encoder object
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le_count = 0
+# Iterate through the columns
+for col in application_train:
+    if application_train[col].dtype == 'object':
+        # If 2 or fewer unique categories
+        if len(list(application_train[col].unique())) <= 2:
+            # Train on the training data
+            le.fit(application_train[col])
+            # Transform both training and testing data
+            application_train[col] = le.transform(application_train[col])
+            application_test[col] = le.transform(application_test[col])
+            
+            # Keep track of how many columns were label encoded
+            le_count += 1
+            
+print('%d columns were label encoded.' % le_count)
+
+
+# one-hot encoding of categorical variables
+application_train = pd.get_dummies(application_train)
+application_test = pd.get_dummies(application_test)
+
+print('Training Features shape: ', application_train.shape)
+print('Testing Features shape: ', application_test.shape)
+
+train_labels = application_train['TARGET']
+
+# Align the training and testing data, keep only columns present in both dataframes
+application_train, application_test = application_train.align(application_test, join = 'inner', axis = 1)
+
+# Add the target back in
+application_train['TARGET'] = train_labels
+
+print('Training Features shape: ', application_train.shape)
+print('Testing Features shape: ', application_test.shape)
+
+
+(application_train['DAYS_BIRTH'] / -365).describe()
+
+application_train['DAYS_EMPLOYED'].describe()
+
+plt.hist(application_train['DAYS_EMPLOYED'])
+#application_train['DAYS_EMPLOYED'].astype(int).plot.hist();
+#(title = 'Days Employment Histogram')
+plt.xlabel('Days Employment')
+
+anom = application_train[application_train['DAYS_EMPLOYED'] == 365243]
+non_anom = application_train[application_train['DAYS_EMPLOYED'] != 365243]
+print('The non-anomalies default on %0.2f%% of loans' % (100 * non_anom['TARGET'].mean()))
+print('The anomalies default on %0.2f%% of loans' % (100 * anom['TARGET'].mean()))
+print('There are %d anomalous days of employment' % len(anom))
+
+
+# Create an anomalous flag column
+application_train['DAYS_EMPLOYED_ANOM'] = application_train["DAYS_EMPLOYED"] == 365243
+
+# Replace the anomalous values with nan
+application_train['DAYS_EMPLOYED'].replace({365243: np.nan}, inplace = True)
+
+application_train['DAYS_EMPLOYED'].plot.hist(title = 'Days Employment Histogram');
+plt.xlabel('Days Employment');
+
+
+application_test['DAYS_EMPLOYED_ANOM'] = application_test["DAYS_EMPLOYED"] == 365243
+application_test["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
+
+print('There are %d anomalies in the test data out of %d entries' % (application_test["DAYS_EMPLOYED_ANOM"].sum(), len(application_test)))
+
+
+# Find correlations with the target and sort
+correlations = application_train.corr()['TARGET'].sort_values()
+
+# Display correlations
+print('Most Positive Correlations:\n', correlations.tail(15))
+print('\nMost Negative Correlations:\n', correlations.head(15))
+
+# Find the correlation of the positive days since birth and target
+application_train['DAYS_BIRTH'] = abs(application_train['DAYS_BIRTH'])
+application_train['DAYS_BIRTH'].corr(application_train['TARGET'])
+
+# Set the style of plots
+plt.style.use('fivethirtyeight')
+
+# Plot the distribution of ages in years
+plt.hist(application_train['DAYS_BIRTH'] / 365, edgecolor = 'k', bins = 25)
+plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
+
+
+plt.figure(figsize = (10, 8))
+
+# KDE plot of loans that were repaid on time
+sns.kdeplot(application_train.loc[application_train['TARGET'] == 0, 'DAYS_BIRTH'] / 365, label = 'target == 0')
+
+# KDE plot of loans which were not repaid on time
+sns.kdeplot(application_train.loc[application_train['TARGET'] == 1, 'DAYS_BIRTH'] / 365, label = 'target == 1')
+
+# Labeling of plot
+plt.xlabel('Age (years)'); plt.ylabel('Density'); plt.title('Distribution of Ages');
+
+
+# Age information into a separate dataframe
+age_data = application_train[['TARGET', 'DAYS_BIRTH']]
+age_data['YEARS_BIRTH'] = age_data['DAYS_BIRTH'] / 365
+
+# Bin the age data
+age_data['YEARS_BINNED'] = pd.cut(age_data['YEARS_BIRTH'], bins = np.linspace(20, 70, num = 11))
+age_data.head(10)
+
+
+# Group by the bin and calculate averages
+age_groups  = age_data.groupby('YEARS_BINNED').mean()
+age_groups
+
+
+plt.figure(figsize = (8, 8))
+
+# Graph the age bins and the average of the target as a bar plot
+age_groups.TARGET.plot(kind='bar')
+# Plot labeling
+plt.xticks(rotation = 75); plt.xlabel('Age Group (years)'); plt.ylabel('Failure to Repay (%)')
+plt.title('Failure to Repay by Age Group');
+
+
+# Extract the EXT_SOURCE variables and show correlations
+ext_data = application_train[['TARGET', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'DAYS_BIRTH']]
+ext_data_corrs = ext_data.corr()
+ext_data_corrs
+
+
+plt.figure(figsize = (8, 6))
+
+# Heatmap of correlations
+sns.heatmap(ext_data_corrs, cmap = plt.cm.RdYlBu_r, vmin = -0.25, annot = True, vmax = 0.6)
+plt.title('Correlation Heatmap');
+
+
+plt.figure(figsize = (10, 12))
+
+# iterate through the sources
+#for i, source in enumerate(['EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3']):
+    
+    # create a new subplot for each source
+ #   plt.subplot(3, 1, i + 1)
+    # plot repaid loans
+  #  sns.kdeplot(application_train.loc[application_train['TARGET'] == 0, source], label = 'target == 0')
+    # plot loans that were not repaid
+   # sns.kdeplot(application_train.loc[application_train['TARGET'] == 1, source], label = 'target == 1')
+    
+    # Label the plots
+    #plt.title('Distribution of %s by Target Value' % source)
+  #  plt.xlabel('%s' % source); plt.ylabel('Density');
+    
+#plt.tight_layout(h_pad = 2.5)
+
+# Groupby the client id (SK_ID_CURR), count the number of previous loans, and rename the column
+previous_loan_counts = bureau.groupby('SK_ID_CURR', as_index=False)['SK_ID_BUREAU'].count().rename(columns = {'SK_ID_BUREAU': 'previous_loan_counts'})
+
+application_train = application_train.merge(previous_loan_counts, on = 'SK_ID_CURR', how = 'left')
+
+application_test = application_test.merge(previous_loan_counts, on = 'SK_ID_CURR', how = 'left')
+
+
+# Fill the missing values with 0 
+application_train['previous_loan_counts'] = application_train['previous_loan_counts'].fillna(0)
+
+# Plots the disribution of a variable colored by value of the target
+def kde_target(var_name, df):
+    
+    # Calculate the correlation coefficient between the new variable and the target
+    corr = df['TARGET'].corr(df[var_name])
+    
+    # Calculate medians for repaid vs not repaid
+    avg_repaid = df.ix[df['TARGET'] == 0, var_name].median()
+    avg_not_repaid = df.ix[df['TARGET'] == 1, var_name].median()
+    
+    plt.figure(figsize = (12, 6))
+    
+    # Plot the distribution for target == 0 and target == 1
+    sns.kdeplot(df.ix[df['TARGET'] == 0, var_name], label = 'TARGET == 0')
+    sns.kdeplot(df.ix[df['TARGET'] == 1, var_name], label = 'TARGET == 1')
+    
+    # label the plot
+    plt.xlabel(var_name); plt.ylabel('Density'); plt.title('%s Distribution' % var_name)
+    plt.legend();
+    
+    # print out the correlation
+    print('The correlation between %s and the TARGET is %0.4f' % (var_name, corr))
+    # Print out average values
+    print('Median value for loan that was not repaid = %0.4f' % avg_not_repaid)
+    print('Median value for loan that was repaid =     %0.4f' % avg_repaid)
+    
+    
+kde_target('EXT_SOURCE_1', application_train)
+kde_target('EXT_SOURCE_2', application_train)
+kde_target('EXT_SOURCE_3', application_train)
+
+
+kde_target('previous_loan_counts', application_train)
+
+kde_target('bureau_DAYS_CREDIT_mean', application_train)
+
+
+#Function for Numeric Aggregations
+
+def agg_numeric(df, group_var, df_name):
+    """Aggregates the numeric values in a dataframe. This can
+    be used to create features for each instance of the grouping variable.
+    
+    Parameters
+    --------
+        df (dataframe): 
+            the dataframe to calculate the statistics on
+        group_var (string): 
+            the variable by which to group df
+        df_name (string): 
+            the variable used to rename the columns
+        
+    Return
+    --------
+        agg (dataframe): 
+            a dataframe with the statistics aggregated for 
+            all numeric columns. Each instance of the grouping variable will have 
+            the statistics (mean, min, max, sum; currently supported) calculated. 
+            The columns are also renamed to keep track of features created.
+    
+    """
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != group_var and 'SK_ID' in col:
+            df = df.drop(col, axis=1)
+            
+    group_ids = df[group_var]
+    numeric_df = df.select_dtypes(include=['number'])
+    numeric_df[group_var] = group_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(group_var).agg(['count', 'mean', 'max', 'min', 'sum']).reset_index()
+
+    # Need to create new column names
+    columns = [group_var]
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        # Skip the grouping variable
+        if var != group_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1][:-1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+    return agg
+
+
+
+# Function to calculate correlations with the target for a dataframe
+def target_corrs(df):
+
+    # List of correlations
+    corrs = []
+
+    # Iterate through the columns 
+    for col in df.columns:
+        print(col)
+        # Skip the target column
+        if col != 'TARGET':
+            # Calculate correlation with the target
+            corr = df['TARGET'].corr(df[col])
+
+            # Append the list as a tuple
+            corrs.append((col, corr))
+            
+    # Sort by absolute magnitude of correlations
+    corrs = sorted(corrs, key = lambda x: abs(x[1]), reverse = True)
+    
+    return corrs
+
+
+#Function to Handle Categorical Variables
+
+def count_categorical(df, group_var, df_name):
+    """Computes counts and normalized counts for each observation
+    of `group_var` of each unique category in every categorical variable
+    
+    Parameters
+    --------
+    df : dataframe 
+        The dataframe to calculate the value counts for.
+        
+    group_var : string
+        The variable by which to group the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+        
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+    
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with counts and normalized counts of each unique category in every categorical variable
+        with one row for every unique value of the `group_var`.
+        
+    """
+    
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes(include=['object']))
+
+    # Make sure to put the identifying id on the column
+    categorical[group_var] = df[group_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(group_var).agg(['sum', 'mean'])
+    
+    column_names = []
+    
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['count', 'count_norm']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+    
+    categorical.columns = column_names
+    
+    return categorical
+
+
+#Bureau Data
+# Group by the client id, calculate aggregation statistics
+bureau_agg_new = agg_numeric(bureau.drop('SK_ID_BUREAU',axis=1), group_var = 'SK_ID_CURR', df_name = 'bureau')
+bureau_agg_new.head()
+
+application_train = application_train.merge(bureau_agg_new, on = 'SK_ID_CURR', how = 'left')
+
+application_test = application_test.merge(bureau_agg_new, on = 'SK_ID_CURR', how = 'left')
+
+
+bureau_counts = count_categorical(bureau, group_var = 'SK_ID_CURR', df_name = 'bureau')
+bureau_counts.head()
+
+
+application_train = application_train.merge(bureau_counts, left_on = 'SK_ID_CURR', right_index = True, how = 'left')
+
+application_test = application_test.merge(bureau_counts, left_on = 'SK_ID_CURR', right_index = True, how = 'left')
+
+
+application_train.shape
+
+application_test.shape
+
+#Value counts of Bureau Balance dataframe by loan
+
+bureau_balance_counts = count_categorical(bureau_balance, group_var = 'SK_ID_BUREAU', df_name = 'bureau_balance')
+bureau_balance_counts.head()
+
+bureau_balance_agg = agg_numeric(bureau_balance, group_var = 'SK_ID_BUREAU', df_name = 'bureau_balance')
+bureau_balance_agg.head()
+
+
+# Dataframe grouped by the loan
+bureau_by_loan = bureau_balance_agg.merge(bureau_balance_counts, right_index = True, left_on = 'SK_ID_BUREAU', how = 'outer')
+
+# Merge to include the SK_ID_CURR
+bureau_by_loan = bureau[['SK_ID_BUREAU', 'SK_ID_CURR']].merge(bureau_by_loan, on = 'SK_ID_BUREAU', how = 'left')
+
+# Aggregate the stats for each client
+bureau_balance_by_client = agg_numeric(bureau_by_loan.drop('SK_ID_BUREAU', axis=1), group_var = 'SK_ID_CURR', df_name = 'client')
+
+
+application_train = application_train.merge(bureau_balance_by_client, on = 'SK_ID_CURR', how = 'left')
+
+application_test = application_test.merge(bureau_balance_by_client, on = 'SK_ID_CURR', how = 'left')
+
+
+missing_train = missing_values_table(application_train)
+missing_train.head(20)
+
+
+
+missing_train_vars = list(missing_train.index[missing_train['% of Total Values'] > 90])
+len(missing_train_vars)
+
+#Aligning test and train data
+
+train_labels = application_train['TARGET']
+
+# Align the dataframes, this will remove the 'TARGET' column
+application_train, application_test = application_train.align(application_test, join = 'inner', axis = 1)
+
+application_train['TARGET'] = train_labels
+
+
+
+missing_test = missing_values_table(application_test)
+missing_test.head(20)
+
+missing_test_vars = list(missing_test.index[missing_test['% of Total Values'] > 90])
+len(missing_test_vars)
+
+
+application_train.to_csv('train_bureau_raw.csv', index = False)
+application_test.to_csv('test_bureau_raw.csv', index = False)
+
+
+#Missing value treatment
+
+from sklearn.preprocessing import MinMaxScaler, Imputer
+
+# Drop the target from the training data
+if 'TARGET' in application_train:
+    train = application_train.drop('TARGET',axis=1)
+else:
+    train = application_train.copy()
+    
+# Feature names
+features = list(train.columns)
+
+# Copy of the testing data
+test = application_test.copy()
+
+# Median imputation of missing values
+imputer = Imputer(strategy = 'median')
+
+# Scale each feature to 0-1
+scaler = MinMaxScaler(feature_range = (0, 1))
+
+# Fit on the training data
+imputer.fit(train)
+
+# Transform both training and testing data
+train = imputer.transform(train)
+test = imputer.transform(application_test)
+
+# Repeat with the scaler
+scaler.fit(train)
+train = scaler.transform(train)
+test = scaler.transform(test)
+
+print('Training data shape: ', train.shape)
+print('Testing data shape: ', test.shape)
+
+from sklearn.ensemble import RandomForestClassifier
+
+# Make the random forest classifier
+random_forest = RandomForestClassifier(n_estimators = 100, random_state = 50, verbose = 1, n_jobs = -1)
+
+# Train on the training data
+random_forest.fit(train, train_labels)
+
+# Extract feature importances
+feature_importance_values = random_forest.feature_importances_
+feature_importances = pd.DataFrame({'feature': features, 'importance': feature_importance_values})
+
+# Make predictions on the test data
+predictions = random_forest.predict_proba(test)[:, 1]
+
+# Make a submission dataframe
+submit = app_test[['SK_ID_CURR']]
+submit['TARGET'] = predictions
+
+# Save the submission dataframe
+
+submit.to_csv('random_forest.csv', index = False)
